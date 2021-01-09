5fcfca8f8c7df876ee8118728d2c07bb7eb740e6
mankamolnar <markmanomolnar@gmail.com>
2021-01-09 01:28:29 -0600

    D01 - Adding for StripePaymentActions.js for creating payment intent

---
+++ src/redux/actions/StripePaymentActions.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+export function createPaymentIntent(paymentDetails) {
+
+  //const HOST = "https://garuda-payment-service.appspot.com";
+  const HOST = "http://localhost:8080";
+
+  return function(dispatch) {
+    dispatch({type: "PAYMENT_INTENT_PENDING"});
+
+    axios.post(HOST + "/create-payment-intent", paymentDetails)
+      .then((response) => {
+        dispatch({type: "PAYMENT_INTENT_FULFILLED", payload: response.data});
+      }).catch((err) => {
+        dispatch({type: "PAYMENT_INTENT_REJECTED", payload: err});
+      });
+  }
+}
