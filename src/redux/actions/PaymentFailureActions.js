fa16bfadb556407c7809e7ce93c8d9222ba7b437
mankamolnar <markmanomolnar@gmail.com>
2021-01-09 01:38:29 -0600

    D01 - Creating PaymentFailureActions to store PaymentIntent objects without succeeded state

---
+++ src/redux/actions/PaymentFailureActions.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+import Back4AppHeaderFactory from "./factories/Back4AppHeaderFactory";
+
+export function createPaymentFailure(objectId, response) {
+  const dispatcher_type = "CREATE_PAYMENT_FAILURE";
+  const back4AppHeaderFactory = new Back4AppHeaderFactory();
+
+  return function(dispatch) {
+    dispatch({type: dispatcher_type + "_PENDING"});
+
+    axios.post(
+      "https://parseapi.back4app.com/classes/PaymentFailure",
+      {
+        applicationId: objectId,
+        response: response
+      },
+      back4AppHeaderFactory.newHeader()
+    ).then((response) => {
+      dispatch({type: dispatcher_type + "_FULFILLED", payload: response.data});
+    }).catch((err) => {
+      dispatch({type: dispatcher_type + "_REJECTED", payload: err});
+    });
+  }
+}
