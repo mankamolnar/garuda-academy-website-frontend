80c343e84f19ef588fec092a8c55f2284218f3f0
mankamolnar <markmanomolnar@gmail.com>
2021-01-09 01:57:26 -0600

    D01 - Adding FormSuccessMessages.js

---
+++ src/components/courses/modal/forms/constants/FormSuccessMessages.js
@@ -0,0 +1,4 @@
+export default Object.freeze({
+  APPLICATION_SUCCESSFUL: "Sikeres jelentkezés! Hamarosan e-mailben küldjük a további tudnivalókat!",
+  APPLICATION_SUCCESSFUL_WITH_WARNING: "Sikeres jelentkezés, de a bankkártyás fizetéssel lehet, hogy probléma adódott. Kollégánk hamarosan felveszi önnel a kapcsolatot!"
+});
