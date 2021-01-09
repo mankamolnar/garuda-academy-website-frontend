c5ef4c7f4f91340ed42d7f4afbc1a6387d4f2758
mankamolnar <markmanomolnar@gmail.com>
2021-01-09 02:17:57 -0600

    D01 - Adding FormatNumberService.js

---
+++ src/components/courses/modal/forms/services/FormatNumberService.js
@@ -0,0 +1,26 @@
+export default class FormatNumberService {
+  addSpacesToNumber = (number) => {
+    const stringNumber = number + "";
+
+    const spacesNeeded = Math.floor(stringNumber.length / 3);
+    const firstNumbers = stringNumber.length % 3;
+
+    let finalNumber = "";
+
+    if (firstNumbers > 0) {
+      finalNumber += stringNumber.substring(0, firstNumbers) + " ";
+    }
+
+    for (let i = 0; i < spacesNeeded; i++) {
+      finalNumber += stringNumber.substring((i * 3) + firstNumbers, (i * 3) + firstNumbers + 3) + " ";
+    }
+
+    finalNumber = finalNumber.substring(0, finalNumber.length - 1);
+
+    return finalNumber;
+  }
+
+  calculatePercentage = (originalNumber, percentage) => {
+    return parseInt(originalNumber) / 100 * percentage;
+  }
+}
