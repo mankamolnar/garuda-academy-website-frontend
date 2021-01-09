346a408dcfadf128faa3ad5ca2442ec39c51868c
mankamolnar <markmanomolnar@gmail.com>
2021-01-09 02:16:38 -0600

    D01 - Adding ReferenceCreatorService.js

---
+++ src/components/courses/modal/forms/services/ReferenceCreatorService.js
@@ -0,0 +1,30 @@
+export default class ReferenceCreatorService {
+  generateReference = (sourceString) => {
+    sourceString = sourceString.toLowerCase();
+    sourceString = this.replaceSpecialLetters(sourceString);
+    sourceString = this.replaceSpecialCharacters(sourceString);
+
+    return sourceString;
+  }
+
+  replaceSpecialLetters = (sourceString) => {
+    sourceString = sourceString.replaceAll("á", "a");
+    sourceString = sourceString.replaceAll("é", "e");
+    sourceString = sourceString.replaceAll("í", "i");
+    sourceString = sourceString.replaceAll("ó", "o");
+    sourceString = sourceString.replaceAll("ö", "o");
+    sourceString = sourceString.replaceAll("ő", "o");
+    sourceString = sourceString.replaceAll("ú", "u");
+    sourceString = sourceString.replaceAll("ü", "u");
+    sourceString = sourceString.replaceAll("ű", "u");
+
+    return sourceString;
+  }
+
+  replaceSpecialCharacters = (sourceString) => {
+    sourceString = sourceString.replaceAll(" ", "-");
+    sourceString = sourceString.replaceAll(".", "");
+    
+    return sourceString;
+  }
+}
