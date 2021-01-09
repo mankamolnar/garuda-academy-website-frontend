5e9f7d34192211584f0a4398fea8a4e58a6c417d
mankamolnar <markmanomolnar@gmail.com>
2021-01-09 01:06:22 -0600

    D01 - A ModalStateActions.js and ModalStateReducer.js to dispatch the visibility of the modal to store

---
+++ src/redux/reducers/ModalStateReducer.js
@@ -0,0 +1,12 @@
+const initialState = {};
+
+export default function reducer(state=initialState, action) {
+  switch (action.type) {
+    case "CHANGE_MODAL_STATE": {
+      return {...action.payload};
+    }
+    default: {
+      return state;
+    }
+  }
+}
