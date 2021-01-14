c5ef1e0328d29cf7d4e73cd6cb45d5af49fa7ee2
mankamolnar <markmanomolnar@gmail.com>
2021-01-14 17:33:08 -0600

    H02 - Into PaymentFormContainer.js adding final stripe key

--- src/components/courses/modal/forms/PaymentFormContainer.js
+++ src/components/courses/modal/forms/PaymentFormContainer.js
@@ -4,8 +4,8 @@ import { Elements } from '@stripe/react-stripe-js';
 import { ElementsConsumer } from '@stripe/react-stripe-js';
 import PaymentForm from '../forms/PaymentForm';
 
-const stripePromise = loadStripe('pk_test_51I1itvINx3tZ5B4LyjNqRfD45nieozEzUEsaPuNFKYJVhiU56POTPexmIiopMDnxvSqQBa33GnBLwws3WMx9SGbi000PcZSmHZ');
-//const stripePromise = loadStripe('pk_live_51I1itvINx3tZ5B4LtMWdlXRxpiIsvz4R0BQaRixnmmRlc3yblkv8g0k79zKntW9amQfy4UfbnfM00h2BKQloxIkF00ekBC7qL0');
+// TODO: add profiles
+const stripePromise = loadStripe('pk_live_51I9KvzFdcBKxPoB2ZrE1XuV3pJG4UjtnMh9GySR8z04cejt33t2D4cCYhXp2KPSrUX4MbuRC9jxpeylZz3e3Otzn00ggnNRctA');
 
 export default (props) => (
   <Elements stripe={stripePromise}>
