36fe78e3d00c9a56ce1d8a7e2bab5f3a65e2ea41
mankamolnar <markmanomolnar@gmail.com>
2021-01-13 18:39:20 -0600

    D02 - Adding new route names to Routes.js

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,9 +1,11 @@
-const routes = {
+const routes = Object.freeze({
   mainPage: '/',
+  childClubs: '/gyerek-szakkorok',
+  summerCamps: '/nyari-taborok',
   courses: '/online-kepzesek',
   privateLessons: '/privat-orak',
   teachers: '/oktatoink',
   contacts: '/kapcsolat'
-};
+});
 
 export default routes;
