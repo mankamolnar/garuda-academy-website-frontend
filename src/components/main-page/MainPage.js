adfd3940ea79b9d2d7061b1e06c9a78f4f8c5673
mankamolnar <markmanomolnar@gmail.com>
2021-01-14 17:23:49 -0600

    H02 - Adding responsive header buttons to MainPage.js

--- src/components/main-page/MainPage.js
+++ src/components/main-page/MainPage.js
@@ -7,7 +7,9 @@ import Boxes from './Boxes';
 import Footer from '../common/footer/Footer';
 import Videos from './Videos';
 import Facebook from '../common/facebook/Facebook';
+import './main-page.css';
 
+// TODO: Header buttons to separate component
 export default class MainPage extends React.Component {
   render() {
     return (
@@ -20,15 +22,27 @@ export default class MainPage extends React.Component {
           </h2>
 
           <div className={"text-center"}>
-            <div className="row">
+            <div className="row header-buttons header-buttons-sd">
               <div className="col-sm-4">
-                <Link to={routes.childClubs} className={"btn btn-success btn-lg mt-4 mr-2 ml-2"}>Gyerek szakkörök</Link>
+                <Link to={routes.childClubs} className={"btn btn-success btn-lg mt-4"}>Gyerek<br />szakkörök</Link>
               </div>
               <div className="col-sm-4">
-                <Link to={routes.summerCamps} className={"btn btn-success btn-lg mt-4 mr-2 ml-2"}>Nyári táborok</Link>
+                <Link to={routes.summerCamps} className={"btn btn-success btn-lg mt-4"}>Nyári<br />táborok</Link>
               </div>
               <div className="col-sm-4">
-                <Link to={routes.privateLessons} className={"btn btn-success btn-lg mt-4 mr-2 ml-2"}>Privát órák</Link>
+                <Link to={routes.privateLessons} className={"btn btn-success btn-lg mt-4"}>Privát<br />órák</Link>
+              </div>
+            </div>
+
+            <div className="row header-buttons header-buttons-hd">
+              <div className="col-sm-4">
+                <Link to={routes.childClubs} className={"btn btn-success btn-lg mt-4"}>Gyerek szakkörök</Link>
+              </div>
+              <div className="col-sm-4">
+                <Link to={routes.summerCamps} className={"btn btn-success btn-lg mt-4"}>Nyári táborok</Link>
+              </div>
+              <div className="col-sm-4">
+                <Link to={routes.privateLessons} className={"btn btn-success btn-lg mt-4"}>Privát órák</Link>
               </div>
             </div>
           </div>
