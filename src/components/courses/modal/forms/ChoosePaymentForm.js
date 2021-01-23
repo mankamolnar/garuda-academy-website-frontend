920321384d527890d33c7e5c8c70c5e257d857c0
mankamolnar <markmanomolnar@gmail.com>
2021-01-23 00:46:35 -0600

    R01-02 - Moved SuccessfulBankTransfer to separate component from ChoosePaymentForm.js

--- src/components/courses/modal/forms/ChoosePaymentForm.js
+++ src/components/courses/modal/forms/ChoosePaymentForm.js
@@ -1,20 +1,15 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PaymentFormContainer from './PaymentFormContainer';
+import SuccessfulBankTransfer from './SuccessfulBankTransfer';
 import { createPaymentMethod } from './../../../../redux/actions/SignUpActions';
-import FormatNumberService from './services/FormatNumberService';
-import ReferenceCreatorService from './services/ReferenceCreatorService';
-import FormSuccessMessages from './constants/FormSuccessMessages';
 import bankCardPaymentImg from './img/bank-card-payment.webp';
 import bankTransferPaymentImg from './img/bank-transfer-payment.webp';
 import './css/choose-payment-form.css';
 
-/** TODO: CHANGE INPUT TO NUMBER WHERE ITS POSSIBLE */
 class ChoosePaymentForm extends React.Component {
   constructor() {
     super();
-    this.formatNumberService = new FormatNumberService();
-    this.referenceCreatorService = new ReferenceCreatorService();
     this.state = {
       step: 'choose'
     }
@@ -40,7 +35,6 @@ class ChoosePaymentForm extends React.Component {
     this.setState({step: "transfer"});
   }
 
-  // TODO: successful render goes to separate component
   render () {
     if (this.state.step === 'choose') {
       return (
@@ -76,41 +70,12 @@ class ChoosePaymentForm extends React.Component {
       return <PaymentFormContainer prevStep={this.setStepToDefault} />;
 
     } else if (this.state.step === "transfer") {
-      const {
-        chosenCourse,
-        signUpInformation
-      } = this.props;
-
-      return (
-        <div>
-          <div className="alert alert-success">
-            { FormSuccessMessages.APPLICATION_SUCCESSFUL }
-          </div>
-
-          <h6>Fizetendő:</h6>
-          <div className="mb-3">
-            { this.formatNumberService.addSpacesToNumber(chosenCourse.amount) } Ft
-          </div>
-
-          <h6>Számla tulajdonos:</h6>
-          <div className="mb-3">Molnár Márk Manó E.V.</div>
-
-          <h6>Számlaszám:</h6>
-          <div className="mb-3">12600016-15776202-07894079</div>
-
-          <h6>Közlemény:</h6>
-          <div className="mb-3">
-            { this.referenceCreatorService.generateReference(signUpInformation.billingName) }
-          </div>
-
-          <h6>Szolgáltató (bank):</h6>
-          <div>Transferwise</div>
-        </div>
-      );
+      return <SuccessfulBankTransfer
+        chosenCourse={this.props.chosenCourse}
+        signUpInformation={this.props.signUpInformation} />;
     }
   }
 };
-
 
 const selector = (store) => {
   return {
