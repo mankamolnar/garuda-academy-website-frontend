9405b614ed8f4f67f5a70cddd6cd78b634f0daa7
mankamolnar <markmanomolnar@gmail.com>
2021-01-23 00:52:40 -0600

    R01-02 - Making stripe to load key from environment variables

--- src/components/courses/modal/forms/PaymentFormContainer.js
+++ src/components/courses/modal/forms/PaymentFormContainer.js
@@ -4,8 +4,7 @@ import { Elements } from '@stripe/react-stripe-js';
 import { ElementsConsumer } from '@stripe/react-stripe-js';
 import PaymentForm from '../forms/PaymentForm';
 
-// TODO: add profiles
-const stripePromise = loadStripe('pk_live_51I9KvzFdcBKxPoB2ZrE1XuV3pJG4UjtnMh9GySR8z04cejt33t2D4cCYhXp2KPSrUX4MbuRC9jxpeylZz3e3Otzn00ggnNRctA');
+const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_LIVE_KEY);
 
 export default (props) => (
   <Elements stripe={stripePromise}>
